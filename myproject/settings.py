f8abcbe0c316e2e8a4f234df5f35d95c029135b2
Rishivarrey <varreyrishi@gmail.com>
2021-10-29 17:14:10 +0530

    setting update

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import django_heroku
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,7 +126,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
